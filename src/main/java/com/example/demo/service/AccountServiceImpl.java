29d280ff5154a165eb810016b3eee4aaa1cc6ecc
Bryce Deyto <bryce.deyto@gmail.com>
2023-08-24 21:16:43 +0800

    chore: added comments
    Added comments for each line
    
    Signed-off-by: Bryce Deyto <bryce.deyto@gmail.com>

--- src/main/java/com/example/demo/service/AccountServiceImpl.java
+++ src/main/java/com/example/demo/service/AccountServiceImpl.java
@@ -65,43 +65,60 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public AccountResponse activateAccount(Long id) {
+//        Retrieve account to be updated
         Account account = repository.findById(id).orElse(new Account());
-//        7
-
+//        Retreive account of sponsor and set it as the current upline
         Account upline = repository.findAccountByAccountIdEquals(account.getSponsorId())
                 .orElse(Account.builder().build());
-//        1
-
+//        Create flag hasUpline and initialize as false
         boolean hasUpline = false;
+//        Create list of accounts
         List<Account> downlines = new ArrayList<>();
-
+//        Create while loop which checks if an upline has been found
         while (!hasUpline) {
+//            Create an account object for the current account to be checked
+//            Current account is from the downlines list or the sponsor for the 1st iteration
             Account current = downlines.stream().findFirst().orElse(upline);
-
+//            Remove current account from the downlines list
             downlines.remove(current);
-
+//            If the current account has no left downlineId execute the following
             if (Optional.ofNullable(current.getLeftDownlineId()).isEmpty()) {
+//                Set hasUpline to true
                 hasUpline = !hasUpline;
+//                Replace the upline with the current account from the iteration
                 upline = current;
+//                Set position of account to be activated as LEFT
                 account.setPosition("LEFT");
+//                Set the uplines' left downline id as the current account id of the account to be activated
                 upline.setLeftDownlineId(account.getAccountId());
+//                Else If the current account has no right downlineId execute the following
             } else if (Optional.ofNullable(current.getRightDownlineId()).isEmpty()) {
+//                Set hasUpline to true
                 hasUpline = !hasUpline;
+//                Replace the upline with the current account from the iteration
                 upline = current;
+//                Set position of account to be activated as RIGHT
                 account.setPosition("RIGHT");
+//                Set the uplines' right downline id as the current account id of the account to be activated
                 upline.setRightDownlineId(account.getAccountId());
             }
-
+//            After looping through the list of downlines, check if the flag above has found a free slot (left or right)
             if (!hasUpline) {
+//                Add the left downline to the current list of downlines
                 downlines.add(repository.findAccountByAccountIdEquals(current.getLeftDownlineId()).orElse(null));
+//                Add the right downline to the current list of downlines
                 downlines.add(repository.findAccountByAccountIdEquals(current.getRightDownlineId()).orElse(null));
+//                If an upline has been found, execute the following
             } else {
+//                Set the current upline id of the account as the account id of the found upline
                 account.setUplineId(upline.getAccountId());
+//                Save the current accounts' new details to db
                 account = repository.save(account);
+//                Save the current uplines' new details to db
                 repository.save(upline);
             }
         }
-
+//        Return response
         return AccountResponse.builder()
                 .id(account.getId())
                 .accountId(account.getAccountId())
